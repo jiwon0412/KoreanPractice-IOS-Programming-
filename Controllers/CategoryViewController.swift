951b886ff5b4f41eb4ed74a1cbc39a151993c174
jiwon <jiwon@naver.com>
2026-05-27 13:39:51 +0900

    feat: CategoryViewController 추가

--- Controllers/CategoryViewController.swift
+++ Controllers/CategoryViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "한국어 연습 🇰🇷"
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+}
 
-
+extension CategoryViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return scenarios.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let scenario = scenarios[indexPath.row]
+        cell.textLabel?.text = "\(scenario.emoji)  \(scenario.title)"
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 18)
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let scenario = scenarios[indexPath.row]
+        // ChatViewController로 이동 (나중에 연결)
+        print("선택: \(scenario.title)")
+    }
 }
 
