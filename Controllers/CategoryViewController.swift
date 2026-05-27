090b0e7a33df7e96a68536a769bc54360e8fe679
jiwon <jiwon@naver.com>
2026-05-27 13:19:27 +0900

    feat: Initial commit

---
+++ Controllers/CategoryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KoreanPractice
+//
+//  Created by 김지원 on 5/27/26.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Views/ChatCell.swift
@@ -0,0 +1,8 @@
+//
+//  ChatCell.swift
+//  KoreanPractice
+//
+//  Created by 김지원 on 5/27/26.
+//
+
+import Foundation
